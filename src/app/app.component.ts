efc07964521427faa48f29d8e48d196d05052ed7
Abhishek07-patane <abhishekpatane5@gmail.com>
2025-09-18 13:20:59 +0530

    Add project at initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from "./hello-world/hello-world.component";
+import { CounterComponent } from "./counter/counter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HelloWorldComponent, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task';
+  
 }
---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.scss'
+})
+export class HelloWorldComponent {
+
+  message = "My_Task";
+
+
+
+}
